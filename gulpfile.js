9d0b011311bc3dfe6b4e85e6128fb3898c0222fc
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2015-11-06 18:38:39 -0600

    Pokesemana wow

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp      = require('gulp'),
+  connect     = require('gulp-connect'),
+  concat      = require('gulp-concat-util'),
+  concatCss   = require('gulp-concat-css'),
+  jade        = require('gulp-jade'),
+  gutil       = require('gulp-util'),
+  gulpif      = require('gulp-if');
+
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'build',
+    port: 8888,
+    livereload: true                                                                                                                                                                                                                           
+  });
+});
+
+gulp.task('jade', function () {
+  return gulp.src(['app/**/*.html', 'app/**/*.jade'])
+    .pipe(gulpif(/[.]jade$/, jade({
+      pretty: true
+    })))
+    .pipe(gulp.dest('build/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+  return gulp.src(["bower_components/**/*.min.css","app/assets/styles/**/*.css"])
+      .pipe(concatCss('app.css'))
+      .pipe(gulp.dest('build/assets/css'))
+});
+
+gulp.task('js', function() {
+  return gulp.src(["bower_components/**/*.min.js", "app/assets/js/**/*.js"])
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('build/assets/js/'))
+    .pipe(connect.reload())
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/assets/**/*.js'], ['js']);
+  gulp.watch(['./app/assets/styles/**/*.css'],['css']);
+  gulp.watch(['./app/**/*.jade'], ['jade']);
+});
+
+gulp.task('default', ['connect', 'jade','css', 'js','watch']);
